8c74297d12b2e31ab5c35ead87b6b5ca3bbb55d1
DhanrajR007 <dhanrajrathore1111@gmail.com>
2025-12-23 10:40:18 +0530

    Refactor hologram shaders to improve normal handling and add glitch effect in vertex shader

--- src/shaders/hologram/fragment.glsl
+++ src/shaders/hologram/fragment.glsl
@@ -7,7 +7,7 @@ varying vec3 vNormal;
 void main() {
    vec3 normal = normalize(vNormal);
    if(!gl_FrontFacing){
-     normal = normal * -1.0;
+     normal *=  -1.0;
    }
 
    float stripes = mod((vPosition.y - uTime * 0.01) * 20.0, 1.0);
@@ -17,12 +17,14 @@ void main() {
    float fresnel = dot(viewDirection , normal) + 1.0;
    fresnel = pow(fresnel, 2.0);
    
+   float fallOff = smoothstep(0.8, 0.0, fresnel);
+   
  
 
    float holographic = stripes * fresnel;
    holographic += fresnel * 1.25;
-   fresnel = smoothstep(0.8, 0.0, fresnel);
-   holographic *= fresnel;
+
+   holographic *= fallOff;
 
     gl_FragColor = vec4(uColor, holographic);
     #include <tonemapping_fragment>
